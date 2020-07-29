35bb8cdd3f450aa0c116fa6b988ac690a4a8c9cc
Abhishek-GH <abhiranjan05@gmail.com>
2020-07-29 14:22:13 +0530

    First commit to Spring Boot

---
+++ src/main/java/com/git/Git_Fundamental/GitFundamentalApplication.java
@@ -0,0 +1,13 @@
+package com.git.Git_Fundamental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitFundamentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitFundamentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/git/Git_Fundamental/GitFundamentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.Git_Fundamental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitFundamentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
